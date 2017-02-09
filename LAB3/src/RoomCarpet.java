388eb74a52098e4f8d2987ca88ca50a5dd612bfb
Luke Creighton <lk479778@dal.ca>
2017-02-08 21:08:46 -0400

    COMMIT 2

--- LAB3/src/RoomCarpet.java
+++ LAB3/src/RoomCarpet.java
@@ -1,4 +1,35 @@
-
 public class RoomCarpet {
+	private RoomDimension room;
+	private double carpetCost, totalCost;
+	
+	public RoomCarpet(RoomDimension r, double c){
+		carpetCost = c;
+		room = r;
+	}
+	// GETTERS AND SETTERS //
+	public RoomDimension getRoom() {
+		return room;
+	}
 
+	public void setRoom(RoomDimension room) {
+		this.room = room;
+	}
+
+	public double getCarpetCost() {
+		return carpetCost;
+	}
+
+	public void setCarpetCost(double carpetCost) {
+		this.carpetCost = carpetCost;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+	// END OF GETTERS AND SETTERS //
+	
 }
